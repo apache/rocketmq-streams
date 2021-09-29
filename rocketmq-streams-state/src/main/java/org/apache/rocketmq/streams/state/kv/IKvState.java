a072ded85338879ba7e20379ba27df19200ee593
write2me <write2me@vip.qq.com>
2021-09-29 14:29:40 +0800

    #74 add LRU state

--- rocketmq-streams-state/src/main/java/org/apache/rocketmq/streams/state/kv/IKvState.java
+++ rocketmq-streams-state/src/main/java/org/apache/rocketmq/streams/state/kv/IKvState.java
@@ -1,4 +1,20 @@
-package org.apache.rocketmq.streams.state;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.streams.state.kv;
 
 import java.util.Iterator;
 import java.util.List;
@@ -10,7 +26,7 @@ import java.util.Set;
  * @param <V>
  * @author arthur.liang
  */
-public interface IStreamState<K, V> {
+public interface IKvState<K, V> {
 
     /**
      * Returns the value to which the specified key is mapped, or {@code null} if this map contains no mapping for the
--- rocketmq-streams-state/src/main/java/org/apache/rocketmq/streams/state/kv/rocksdb/RocksdbState.java
+++ rocketmq-streams-state/src/main/java/org/apache/rocketmq/streams/state/kv/rocksdb/RocksdbState.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.rocketmq.streams.state.kv.rocksdb;
 
 import java.io.UnsupportedEncodingException;
@@ -10,7 +26,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.rocketmq.streams.common.utils.CollectionUtil;
 import org.apache.rocketmq.streams.common.utils.StringUtil;
-import org.apache.rocketmq.streams.state.IStreamState;
+import org.apache.rocketmq.streams.state.LruState;
+import org.apache.rocketmq.streams.state.kv.IKvState;
 import org.rocksdb.ReadOptions;
 import org.rocksdb.RocksDBException;
 import org.rocksdb.RocksIterator;
@@ -24,19 +41,18 @@ import static org.apache.rocketmq.streams.state.kv.rocksdb.RocksDBOperator.UTF8;
  *
  * @author arthur.liang
  */
-public class RocksdbState implements IStreamState<java.lang.String, java.lang.String> {
+public class RocksdbState implements IKvState<String, String> {
 
     private static RocksDBOperator operator = new RocksDBOperator();
 
-    //TODO replace
-    private HashMap<String,Byte> cache = new HashMap<>();
+    private final LruState<String> cache = new LruState<>(100, "");
+
     private final static Byte SIGN = 1;
 
     @Override public String get(String key) {
         try {
             return getValueFromByte(operator.getInstance().get(getKeyBytes(key)));
         } catch (Exception e) {
-            //throw new RuntimeException("failed in getting key from rocksdb! " + key, e);
             return null;
         }
     }
@@ -76,12 +92,11 @@ public class RocksdbState implements IStreamState<java.lang.String, java.lang.St
     }
 
     @Override public String putIfAbsent(String key, String value) {
-        //TODO use lru cache
-        if (cache.containsKey(key)) {
+        if (cache.search(key) <= 0) {
             return null;
         }
         put(key, value);
-        cache.put(key, SIGN);
+        cache.add(key);
         return null;
     }
 
