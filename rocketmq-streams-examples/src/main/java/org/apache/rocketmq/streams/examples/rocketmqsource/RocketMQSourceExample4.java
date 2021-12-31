b344c8f18846a57a838eb6b4ad470d6ec39ad2c2
维章 <unclerangoo@gmail.com>
2021-12-31 10:21:53 +0800

    fix(RocketMQSourceExample4) join window

--- rocketmq-streams-examples/src/main/java/org/apache/rocketmq/streams/examples/rocketmqsource/RocketMQSourceExample4.java
+++ rocketmq-streams-examples/src/main/java/org/apache/rocketmq/streams/examples/rocketmqsource/RocketMQSourceExample4.java
@@ -1,14 +1,12 @@
 package org.apache.rocketmq.streams.examples.rocketmqsource;
 
-import com.alibaba.fastjson.JSONObject;
 import org.apache.rocketmq.streams.client.StreamBuilder;
+import org.apache.rocketmq.streams.client.strategy.WindowStrategy;
 import org.apache.rocketmq.streams.client.transform.DataStream;
 import org.apache.rocketmq.streams.client.transform.JoinStream;
-import org.apache.rocketmq.streams.common.functions.FilterFunction;
+import org.apache.rocketmq.streams.client.transform.window.Time;
 
-import static org.apache.rocketmq.streams.examples.rocketmqsource.Constant.NAMESRV_ADDRESS;
-import static org.apache.rocketmq.streams.examples.rocketmqsource.Constant.RMQ_CONSUMER_GROUP_NAME;
-import static org.apache.rocketmq.streams.examples.rocketmqsource.Constant.RMQ_TOPIC;
+import static org.apache.rocketmq.streams.examples.rocketmqsource.Constant.*;
 
 public class RocketMQSourceExample4 {
 
@@ -25,27 +23,26 @@ public class RocketMQSourceExample4 {
 
         System.out.println("begin streams code");
 
-        DataStream leftStream = StreamBuilder.dataStream("namespace", "name").fromRocketmq(RMQ_TOPIC,
+        DataStream leftStream = StreamBuilder.dataStream("namespace", "name").fromRocketmq(
+                RMQ_TOPIC,
                 RMQ_CONSUMER_GROUP_NAME,
-                false,
-                NAMESRV_ADDRESS)
-                .filter((FilterFunction<String>) value -> JSONObject.parseObject(value).getString("InFlow") != null);
+                true,
+                NAMESRV_ADDRESS);
 
 
-        DataStream rightStream = StreamBuilder.dataStream("namespace", "name2").fromRocketmq(RMQ_TOPIC + 2,
+        DataStream rightStream = StreamBuilder.dataStream("namespace", "name2").fromRocketmq(
+                RMQ_TOPIC + 2,
                 RMQ_CONSUMER_GROUP_NAME + 2,
-                false,
-                NAMESRV_ADDRESS)
-                .filter((FilterFunction<String>) value -> JSONObject.parseObject(value).getString("InFlow") != null);
+                true,
+                NAMESRV_ADDRESS);
 
-        leftStream.join(rightStream).setJoinType(JoinStream.JoinType.LEFT_JOIN)
+        leftStream.join(rightStream)
+                .setJoinType(JoinStream.JoinType.LEFT_JOIN)
                 .setCondition("InFlow,==,InFlow")
+                .window(Time.minutes(1))
                 .toDataSteam()
-                .map(message -> {
-                    System.out.println(message);
-                    return message + "--";
-                })
                 .toPrint(1)
+                .with(WindowStrategy.highPerformance())
                 .start();
 
         System.out.println("consumer end");
