3dbb82143a22df0c7b6c4c106ce8f13401a80fdc
渊澄 <57705510+yuancheng314@users.noreply.github.com>
2022-06-24 16:55:24 +0800

    filter scores

--- rocketmq-streams-examples/src/main/java/org/apache/rocketmq/streams/examples/source/FileSourceExample.java
+++ rocketmq-streams-examples/src/main/java/org/apache/rocketmq/streams/examples/source/FileSourceExample.java
@@ -18,13 +18,27 @@ package org.apache.rocketmq.streams.examples.source;
 
 import org.apache.rocketmq.streams.client.StreamBuilder;
 import org.apache.rocketmq.streams.client.source.DataStreamSource;
+import org.json.JSONObject;
 
 public class FileSourceExample {
     public static void main(String[] args) {
         DataStreamSource source = StreamBuilder.dataStream("namespace", "pipeline");
         source.fromFile("data.txt", false)
-            .map(message -> message)
-            .toPrint(1)
-            .start();
+                .map(message -> message)
+                .toPrint(1)
+                .start();
+
+        try {
+            Thread.sleep(1000 * 5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        source.fromFile("studentScores.txt", true)
+                .map(message -> message)
+                .filter(message -> ((JSONObject) message).getInt("score") > 90)
+                .selectFields("name","subject")
+                .toFile("./rocketmq-streams-examples/src/main/resources/result.txt")
+                .start();
     }
 }
