bfd8a961aae5c94b64dd96a36e7116f0a5ea5808
维章 <unclerangoo@gmail.com>
2022-09-09 15:55:38 +0800

    fix(custom function) support utf

--- rocketmq-streams-channel-rocketmq/src/main/java/org/apache/rocketmq/streams/sink/RocketMQSink.java
+++ rocketmq-streams-channel-rocketmq/src/main/java/org/apache/rocketmq/streams/sink/RocketMQSink.java
@@ -18,7 +18,6 @@
 package org.apache.rocketmq.streams.sink;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
